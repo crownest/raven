f2a51e55088cd9ee0bd1231f4d547f0281ea1347
semsinur <shemsinur.enver@gmail.com>
2017-08-02 17:35:02 +0300

    refs #2: colleges models

---
+++ colleges/admin.py
@@ -0,0 +1,10 @@
+# Django
+from django.contrib import admin
+# Local Django
+from colleges.models import College
+
+@admin.register(College)
+class CollegeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone_number')
+    list_filter = ('name',)
+    search_fields = ('name',)
---
+++ colleges/models.py
@@ -0,0 +1,14 @@
+# Django
+from django.db import models
+
+class College(models.Model):
+    name = models.CharField(verbose_name='Name', max_length=50, unique=True)
+    address = models.TextField(verbose_name="Address", blank=True)
+    phone_number = models.CharField(verbose_name="Phone Number", max_length=50, blank=True)
+
+    class Meta:
+        verbose_name = 'College'
+        verbose_name_plural = 'Colleges'
+
+    def __str__(self):
+        return '{name}'.format(name=self.name)
